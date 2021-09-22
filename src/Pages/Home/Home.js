750f4fb9cbbcf24a30469a5220a839d055c06e48
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-09-21 19:12:42 -0700

    location updates

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,26 +3,27 @@ import './Home.css'
 import Typewriter from 'typewriter-effect';
 
 function Home() {
-  return (
-      <div className="home">
-        <h1 className="home_h1">Microblading near me in Corona CA</h1>
-        <h2 className="home_h2">Best PMU in Riverside, CA</h2>
-        <h3 className="home_h2">Best Lip Blushing in Corona, CA</h3>
-       <Typewriter 
-       onInit = {(typewriter) => {
-         typewriter
-         .typeString("PMU Services In Lake Elsinore, Riverside!")
-         .pauseFor(1000)
-         .deleteAll()
-         .typeString("Lip Blush, Powder Brows, Microblading!")
-         .pauseFor(1000)
-         .deleteAll()
-         .typeString("LET'S ENHANCE AND HIGHLIGHT YOUR NATURAL BEAUTY!")
-         .start()
-       }}
-       />
-      </div>
-  )
+    return (
+        <div className="home">
+          <h1 className="home_h1">Microblading near me in Corona CA</h1>
+          <h1 className="home_h1">Microblading near me in Lake Elsinore CA</h1>
+          <h2 className="home_h2">Best PMU in Riverside, CA</h2>
+          <h3 className="home_h2">Best Lip Blushing in Corona, CA</h3>
+         <Typewriter 
+         onInit = {(typewriter) => {
+           typewriter
+           .typeString("PMU Services In Lake Elsinore, Riverside!")
+           .pauseFor(1000)
+           .deleteAll()
+           .typeString("Lip Blush, Powder Brows, Microblading!")
+           .pauseFor(1000)
+           .deleteAll()
+           .typeString("LET'S ENHANCE AND HIGHLIGHT YOUR NATURAL BEAUTY!")
+           .start()
+         }}
+         />
+        </div>
+    )
 }
 
 export default Home
